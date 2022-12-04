e0f73f29dc46afcd4aefd9c6a71bca9417bd80cd
MagicPrince666 <846863428@qq.com>
2022-12-04 19:05:54 +0800

    修复小问题

--- moto/pwm.cc
+++ moto/pwm.cc
@@ -1,20 +1,20 @@
-#include <stdlib.h>
-#include <unistd.h>
-#include <strings.h>
-#include <string.h>
-#include <sys/types.h>
 #include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <sys/ioctl.h>
-#include <signal.h>
 #include <iostream>
+#include <signal.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #include "pwm.h"
 
 Pwm::Pwm(int pwm) : pwm_pin_(pwm)
 {
-    setup_pwm(pwm); //pwm0
+    setup_pwm(pwm); // pwm0
     pwm_polarity(false);
     pwm_period(20000000);
     pwm_duty_cycle(1500000);
@@ -33,7 +33,7 @@ int Pwm::setup_pwm(int pin)
     if (set_export == NULL) {
         printf("Can\'t open /sys/class/pwm/pwmchip0/export\n");
     } else {
-        fprintf(set_export, setpin_);
+        fprintf(set_export, "%s", setpin_);
     }
     fclose(set_export);
     return 0;
@@ -42,9 +42,9 @@ int Pwm::setup_pwm(int pin)
 int Pwm::pwm_enable()
 {
     FILE *set_enable = NULL;
-    int len = sprintf(setpin_, "/sys/class/pwm/pwmchip0/pwm%d/enable", pwm_pin_);
-    setpin_[len] = 0;
-    set_enable = fopen(setpin_, "w");
+    int len          = sprintf(setpin_, "/sys/class/pwm/pwmchip0/pwm%d/enable", pwm_pin_);
+    setpin_[len]     = 0;
+    set_enable       = fopen(setpin_, "w");
     if (set_enable == NULL) {
         printf("open %s error\n", setpin_);
     } else {
@@ -57,9 +57,9 @@ int Pwm::pwm_enable()
 int Pwm::pwm_disable()
 {
     FILE *set_enable = NULL;
-    int len = sprintf(setpin_, "/sys/class/pwm/pwmchip0/pwm%d/enable", pwm_pin_);
-    setpin_[len] = 0;
-    set_enable = fopen(setpin_, "w");
+    int len          = sprintf(setpin_, "/sys/class/pwm/pwmchip0/pwm%d/enable", pwm_pin_);
+    setpin_[len]     = 0;
+    set_enable       = fopen(setpin_, "w");
     if (set_enable == NULL) {
         printf("open %s error\n", setpin_);
     } else {
@@ -72,21 +72,15 @@ int Pwm::pwm_disable()
 int Pwm::pwm_polarity(bool value)
 {
     FILE *set_polarity = NULL;
-    int len = sprintf(setpin_, "/sys/class/pwm/pwmchip0/pwm%d/polarity", pwm_pin_);
-    setpin_[len] = 0;
-    set_polarity = fopen(setpin_, "w");
-    if (set_polarity == NULL)
-    {
+    int len            = sprintf(setpin_, "/sys/class/pwm/pwmchip0/pwm%d/polarity", pwm_pin_);
+    setpin_[len]       = 0;
+    set_polarity       = fopen(setpin_, "w");
+    if (set_polarity == NULL) {
         printf("open %s error\n", setpin_);
-    }
-    else
-    {
-        if (!value)
-        {
+    } else {
+        if (!value) {
             fprintf(set_polarity, "normal");
-        }
-        else
-        {
+        } else {
             fprintf(set_polarity, "inversed");
         }
     }
@@ -97,15 +91,12 @@ int Pwm::pwm_polarity(bool value)
 int Pwm::pwm_period(int value)
 {
     FILE *set_period = NULL;
-    int len = sprintf(setpin_, "/sys/class/pwm/pwmchip0/pwm%d/period", pwm_pin_);
-    setpin_[len] = 0;
-    set_period = fopen(setpin_, "w");
-    if (set_period == NULL)
-    {
+    int len          = sprintf(setpin_, "/sys/class/pwm/pwmchip0/pwm%d/period", pwm_pin_);
+    setpin_[len]     = 0;
+    set_period       = fopen(setpin_, "w");
+    if (set_period == NULL) {
         printf("open %s error\n", setpin_);
-    }
-    else
-    {
+    } else {
         fprintf(set_period, "%d", value);
     }
     fclose(set_period);
@@ -115,15 +106,12 @@ int Pwm::pwm_period(int value)
 int Pwm::pwm_duty_cycle(int value)
 {
     FILE *set_duty_cycle = NULL;
-    int len = sprintf(setpin_, "/sys/class/pwm/pwmchip0/pwm%d/duty_cycle", pwm_pin_);
-    setpin_[len] = 0;
-    set_duty_cycle = fopen(setpin_, "w");
-    if (set_duty_cycle == NULL)
-    {
+    int len              = sprintf(setpin_, "/sys/class/pwm/pwmchip0/pwm%d/duty_cycle", pwm_pin_);
+    setpin_[len]         = 0;
+    set_duty_cycle       = fopen(setpin_, "w");
+    if (set_duty_cycle == NULL) {
         printf("open %s error\n", setpin_);
-    }
-    else
-    {
+    } else {
         fprintf(set_duty_cycle, "%d", value);
     }
     fclose(set_duty_cycle);
